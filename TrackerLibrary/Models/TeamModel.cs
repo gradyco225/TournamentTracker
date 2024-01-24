0960226088217bb57eecf0762840374ea04eab84
gradyco225 <gradyco225@gmail.com>
2024-01-24 16:49:44 -0500

    Adding code

---
+++ TrackerLibrary/Models/TeamModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary.Models
+{
+    public class TeamModel
+    {
+        /// <summary>
+        /// The unique identifier for the team.
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string TeamName { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<PersonModel> TeamMembers { get; set; } = new List<PersonModel>();
+    }
+}
